aacbf28c18f39621eaf76c08025fe0dd183e22c2
Ellery Queen <429889664@qq.com>
2015-10-30 23:00:57 +0800

    20151030 update
    main structure

---
+++ Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kururu on 2015/10/29.
+ */
+public class Main {
+    static calc calculator = new calc();
+    public static void main(String[] args) {
+        calculator.setVisible(true);
+    }
+}
