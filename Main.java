9a180f7a631171b0d8978da30fdda70e0cc94976
Ellery Queen <429889664@qq.com>
2015-10-30 23:01:46 +0800

    Revert "20151030 update"
    This reverts commit aacbf28c18f39621eaf76c08025fe0dd183e22c2.

--- Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by kururu on 2015/10/29.
- */
-public class Main {
-    static calc calculator = new calc();
-    public static void main(String[] args) {
-        calculator.setVisible(true);
-    }
-}
